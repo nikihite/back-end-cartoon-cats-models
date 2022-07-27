29493841707af5817bb3e1a9056c83cf53371feb
Niki Hite <nikihite@comcast.net>
2022-07-27 15:06:09 -0700

    boolean changed to snake case and tests passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -7,12 +7,12 @@ CREATE table cats (
     name VARCHAR NOT NULL,
     type VARCHAR NOT NULL,
     url VARCHAR NOT NULL,
-    year BIGINT,
-    lives BIGINT,
-    isSidekick BOOLEAN NOT NULL
+    year INT,
+    lives INT,
+    is_sidekick BOOLEAN
 );
 
-INSERT INTO cats (name, type, url, year, lives, isSidekick) VALUES
+INSERT INTO cats (name, type, url, year, lives, is_sidekick) VALUES
 ('Felix', 'Tuxedo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png', 1892, 3, false),
 ('Garfield', 'Orange Tabby', 'https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg', 1978, 7, false),
 ('Duchess', 'Angora', 'https://static.wikia.nocookie.net/disney/images/e/eb/Profile_-_Duchess.jpeg', 1970, 9, false),
